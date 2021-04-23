16091daf2e8a35f8e515f164a114b5325ca6f11d
ContrerasEsparzaJoe123 <47192243+ContrerasEsparzaJoe123@users.noreply.github.com>
2021-04-23 15:23:18 -0500

    added chat function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,35 @@
 import React from "react";
-
+import GoogleLogin from "react-google-login";
+import swal from "sweetalert"
 class Login extends React.Component {
+
   render() {
+    const respuesta_exito=()=> {
+      swal({
+        title: "Bienvenido",
+        text: "Inicio de Sesion con exito",
+        icon: "success",
+        button:"Aceptar"
+    })
+    }
+    const respuesta_fallo = () => {
+      swal({
+        title: "Error al iniciar sesion",
+        text: "Intentalo de nuevo",
+        icon: "error",
+        button:"Aceptar"
+    })
+    }
     return (
       <div>
         <center>
-          <h1>Contreras Esparza joe</h1>
+          <GoogleLogin
+            clientId="69419251981-01ase98llsa4kls1r18kmmut93kkg68f.apps.googleusercontent.com"
+            buttonText="Inicio de Sesion"
+            onSuccess={respuesta_exito}
+            onFailure={respuesta_fallo}
+            cookiePolicy={"single_host_origin"}
+          />
         </center>
       </div>
     );
